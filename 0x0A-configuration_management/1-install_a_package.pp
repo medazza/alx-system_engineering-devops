c73b3174030c9e10dc1f7dedaefe31af22fe7c5b
medazza <midazza27@gmail.com>
2024-01-19 12:50:53 -0800

    task 1 update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,17 @@
 #!usr/bin/pup
 # Install a specific version of Flask
 package {'python3-pip':
-	ensure => installed,
+	ensure	=> installed,
 }
 
 package {'flask':
-	ensure 	 => '2.1.0',
-	provider => 'pip',
-	require => Package['python3-pip'],
+	ensure	=> '2.1.0',
+	provider	=> 'pip',
+	require	=> Package['python3-pip'],
 }
 
 package {'werkzeug':
-	ensure => '2.1.0',
-	provider => 'pip',
-	require => Package['python3-pip'],
+	ensure	=> '2.1.0',
+	provider	=> 'pip',
+	require	=> Package['python3-pip'],
 }
