c4fb9e16a3c55d6ab7f12fa879526a3a1ffaf73c
medazza <midazza27@gmail.com>
2024-02-19 14:57:41 -0800

    task 1 api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,37 +9,19 @@ import sys
 def fetch_employee_todo_progress(empId):
     if empId == 0:
         sys.exit(1)
-    # API endpoint for fetching user data
-    user_url = f'https://jsonplaceholder.typicode.com/users/{empId}'
-    # API endpoint for fetching TODO data
-    todo_url = f'https://jsonplaceholder.typicode.com/todos?userId={empId}'
-
-    # Fetch user data
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("Error: Unable to fetch user data")
-        sys.exit(1)
-    user_data = user_response.json()
-
-    # Fetch TODO data
-    todo_response = requests.get(todo_url)
-    if todo_response.status_code != 200:
-        print("Error: Unable to fetch todos data")
-        sys.exit(1)
-    todo_data = todo_response.json()
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_data = requests.get(f"{base_url}users/{empId}").json()
+    todo_data = requests.get(f"{base_url}todos", params={"userId": empId}).json()
 
     # Extract relevant information
     employee_name = user_data.get('name')
-    user_id = user_data.get('id')
-    total_tasks = len(todo_data)
-    done_tasks = [task for task in todo_data if task.get('completed') is True]
 
     # Exporting to CSV FILE
-    with open(f"{user_id}.csv", "w", newline="") as csvfile:
+    with open(f"{empId}.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for todo in todo_data:
             writer.writerow(
-                [user_id, employee_name, todo["completed"], todo["title"]]
+                [empId, employee_name, todo["completed"], todo["title"]]
             )
 
 
