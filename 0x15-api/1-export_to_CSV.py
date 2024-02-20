12a7f65e41ba0f01a2f3de2b52134ae9edbd9c08
medazza <midazza27@gmail.com>
2024-02-20 11:04:33 -0800

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Write a Python script that, using this REST API, for a given employee ID
-    export data in the CSV format."""
+    returns information about his/her TODO list progress."""
 import csv
 import requests
 import sys
@@ -10,50 +10,45 @@ def fetch_employee_todo_progress(empId):
     if empId == 0:
         sys.exit(1)
     # API endpoint for fetching user data
-    user_url = f'https://jsonplaceholder.typicode.com/users/{empId}'
+    USER_URL = f'https://jsonplaceholder.typicode.com/users/{empId}'
     # API endpoint for fetching TODO data
-    todo_url = f'https://jsonplaceholder.typicode.com/todos?userId={empId}'
+    TODO_URL = f'https://jsonplaceholder.typicode.com/todos?userId={empId}'
 
-    # Fetch user data
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
+    # Fetching user data
+    USER_RESPONSE = requests.get(USER_URL)
+    if USER_RESPONSE.status_code != 200:
         print("Error: Unable to fetch user data")
         sys.exit(1)
-    user_data = user_response.json()
-
-    # Fetch TODO data
-    todo_response = requests.get(todo_url)
-    if todo_response.status_code != 200:
+    USER_DATA = USER_RESPONSE.json()
+    # Fetching TODO data
+    TODO_RESPONSE = requests.get(TODO_URL)
+    if TODO_RESPONSE.status_code != 200:
         print("Error: Unable to fetch todos data")
         sys.exit(1)
-    todo_data = todo_response.json()
-
-    # Extract relevant information
-    employee_name = user_data.get('name')
-    user_id = user_data.get('id')
-    total_tasks = len(todo_data)
-    done_tasks = [task for task in todo_data if task.get('completed') is True]
+    TODO_DATA = TODO_RESPONSE.json()
+    # Extracting relevant information
+    EMPLOYEE_NAME = USER_DATA.get('name')
+    USER_ID = USER_DATA.get('id')
+    TOTAL_TASKS = len(TODO_DATA)
+    DONE_TASKS = [task for task in TODO_DATA if task.get('completed') is True]
 
     # Exporting to CSV
-    CSV_FILE_NAME = f"{user_id}.csv"
+    CSV_FILE_NAME = f"{USER_ID}.csv"
     with open(CSV_FILE_NAME, mode='w', newline='') as csv_file:
         fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
                       'TASK_TITLE']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
         # Writing CSV header
         writer.writeheader()
-
         # Writing CSV rows
-        for task in todo_data:
+        for task in TODO_DATA:
             writer.writerow({
-                'USER_ID': user_id,
-                'USERNAME': employee_name,
-                'TASK_COMPLETED_STATUS': 'TRUE' if task['completed']
-                else 'FALSE',
+                'USER_ID': USER_ID,
+                'USERNAME': EMPLOYEE_NAME,
+                'TASK_COMPLETED_STATUS': 'completed' if task.get('completed')
+                else 'not completed',
                 'TASK_TITLE': task['title']
             })
-
     print(f"\nData exported to {CSV_FILE_NAME}")
 
 
@@ -62,9 +57,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 script_name.py <employee_id>")
         sys.exit(1)
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
-    fetch_employee_todo_progress(employee_id)
+
+    EMPLOYEE_ID = int(sys.argv[1])
+    fetch_employee_todo_progress(EMPLOYEE_ID)
