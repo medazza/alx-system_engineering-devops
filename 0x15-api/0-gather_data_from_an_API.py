a3ec418f27ce58dc776a25dee45f7eff73e3ff06
medazza <midazza27@gmail.com>
2024-02-20 10:57:40 -0800

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-'''
-    Python script that returns information using REST API
-'''
+""" Write a Python script that, using this REST API, for a given employee ID
+    returns information about his/her TODO list progress."""
 import requests
 import sys
 
 
-def FETCH_EMPLOYEE_TODO_PROGRESS(empId):
+def fetch_employee_todo_progress(empId):
     if empId == 0:
         sys.exit(1)
     # API endpoint for fetching user data
@@ -16,24 +15,28 @@ def FETCH_EMPLOYEE_TODO_PROGRESS(empId):
 
     # Fetching user data
     USER_RESPONSE = requests.get(USER_URL)
+    if USER_RESPONSE.status_code != 200:
+        print("Error: Unable to fetch user data")
+        sys.exit(1)
     USER_DATA = USER_RESPONSE.json()
-
     # Fetching TODO data
     TODO_RESPONSE = requests.get(TODO_URL)
+    if TODO_RESPONSE.status_code != 200:
+        print("Error: Unable to fetch todos data")
+        sys.exit(1)
     TODO_DATA = TODO_RESPONSE.json()
-
     # Extracting relevant information
-    EMPLOYEE_NAME = USER_DATA['name']
+    EMPLOYEE_NAME = USER_DATA.get('name')
     TOTAL_TASKS = len(TODO_DATA)
-    COMPLETED_TASKS = [task for task in TODO_DATA if task['completed']]
+    DONE_TASKS = [task for task in TODO_DATA if task.get('completed') is True]
 
     # Displaying the progress
     print(f"Employee {EMPLOYEE_NAME} is done with "
-          f"tasks({len(COMPLETED_TASKS)}/{TOTAL_TASKS}):")
+          f"tasks({len(DONE_TASKS)}/{TOTAL_TASKS}):")
 
     # Displaying titles of completed tasks
-    for task in COMPLETED_TASKS:
-        print(f"\t{task['title']}")
+    for task in DONE_TASKS:
+        print(f"\t{task.get('title')}")
 
 
 if __name__ == "__main__":
@@ -43,4 +46,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     EMPLOYEE_ID = int(sys.argv[1])
-    FETCH_EMPLOYEE_TODO_PROGRESS(EMPLOYEE_ID)
+    fetch_employee_todo_progress(EMPLOYEE_ID)
