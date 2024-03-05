b996be87fcdeffceec5c5f51ce2c29f069402ea3
medazza <midazza27@gmail.com>
2024-03-05 21:24:39 +0100

    task 0 +

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,9 @@ Requirements:
     Prototype: def number_of_subscribers(subreddit)
     If not a valid subreddit, return 0.
 """
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
@@ -12,8 +14,6 @@ def number_of_subscribers(subreddit):
         Function that queries the Reddit API and returns
         the number of subscribers (not acrive users, total subscibers)
     """
-    if subreddit is None or type(subreddit) is not str:
-        return 0
     user_agent = '/u/alx API Python for Holberton School'
     headers = {'user-agent': user_agent}
 
@@ -22,7 +22,8 @@ def number_of_subscribers(subreddit):
     client = requests.session()
     client.headers = headers
 
-    response = requests.get(url, allow_redirects=False)
+    response = client.get(url, allow_redirects=False)
+    
     if response.status_code == 200:
         return response.json()["data"]["subscribers"]
     elif response.status_code == 404:
