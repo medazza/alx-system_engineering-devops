b09e227248a7b9c887be55ffa815b360bb836ab1
medazza <midazza27@gmail.com>
2024-03-05 21:34:30 +0100

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
-"""
-Requirements:
-    Prototype: def number_of_subscribers(subreddit)
-    If not a valid subreddit, return 0.
-"""
-import json
+""" get redit number of subscribers"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-        Function that queries the Reddit API and returns
-        the number of subscribers (not acrive users, total subscibers)
-    """
-    user_agent = '/u/alx API Python for Holberton School'
-    headers = {'user-agent': user_agent}
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    client = requests.session()
-    client.headers = headers
-
-    response = client.get(url, allow_redirects=False)
-    
-    if response.status_code == 200:
-        return response.json()["data"]["subscribers"]
-    elif response.status_code == 404:
-        print(f"Subreddit '{subreddit}' not found.")
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
     else:
-        print(f"Error: {response.status_code}")
         return 0
