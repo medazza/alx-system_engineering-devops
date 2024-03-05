7a88f12451bb5d3bf27c0f2a7e69fae811a5bf35
medazza <midazza27@gmail.com>
2024-03-05 21:44:26 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
+    headers = {"User-Agent": "MyRedditBot/1.0"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        return data.get("data").get("subscribers")
     else:
         return 0
